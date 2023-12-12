487b2507c7030a6d8d594f910d3efab73db3d352
iamkoji <153569512+iamkoji@users.noreply.github.com>
2023-12-12 02:47:12 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">Koji's Discord Bio</h1>
+<p align="center">
+
+This is a personal website, credits below, I decided to add on more features to make it more modern.
+
+Additions:
+
+- "Click to enter!" Transition with a smooth fade
+- On click, it will play music to enhance user experience.
+- Added "Ative Developer Badge" Since the original one din't have it
+
+    
+### ✨ Credits to Phineas for the discord display check it out below:
+
+- ✅ https://github.com/theosanct0s/discord.bio!
---
+++ assets/js/config.js
@@ -0,0 +1,39 @@
+const BADGES = [
+  {
+    name: "HypeSquad Balance", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_balance.svg",
+  },
+  {
+    name: "Active Developer", // Message that appears when you hover over it
+    image: "assets/badges/Discord_Active_Developer_Badge.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "Subscriber since Nov 5, 2023",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Nov 6, 2023",
+    image: "assets/badges/boost.svg",
+  }
+];
+
+const USER_ID = "1032352191131955201"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/bg.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
